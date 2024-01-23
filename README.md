e7d69b2636a1efeb64a0943e9a957ca65e3fdbf3
Manxi1 <muhirwag@spu.edu>
2024-01-22 19:43:06 -0800

    v2

---
+++ README.md
@@ -0,0 +1,14 @@
+# Team Members
+
+1. Manxi Muhirwa
+     Github user:Manxi1
+2. Weston Hanson
+    Github user:WestonHanson
+3. Caed Osborn-Nienhuis
+    Github user:
+# App Name
+    -SipLog
+
+# About
+This app help will user track their  drinks they took through out the day this app will track things like calories , caffein and many more , the app will designing using Js React native and it will run in the Androids. 
+ 
---
+++ components/Drinks.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+
+const Drink = (props) => {
+  let textToSplit = props.text || '';
+  if (typeof textToSplit !== 'string') {
+    textToSplit = String(textToSplit); // Convert to string if not already a string
+  }
+
+  let [drink, volume] = textToSplit.split(' ');
+
+  if (!volume) {
+    drink = textToSplit;
+    volume = '';
+  }
+
+  const handleDeleteItem = () => {
+    props.handleDelete(props.id); // Call the handleDelete function with the appropriate ID
+  };
+
+  return (
+    <View style={styles.item}>
+      <View style={styles.itemLeft}>
+        <TouchableOpacity onPress={handleDeleteItem}>
+          <View style={styles.square}>
+            <Icon name="trash-alt" size={24} color="#000000" />
+          </View>
+        </TouchableOpacity>
+        <Text style={styles.itemText}>{drink}</Text>
+        {!!volume && <Text style={styles.itemText}> {volume}ml</Text>}
+      </View>
+      <View style={styles.menu}>
+        <TouchableOpacity onPress={() => console.log('Menu clicked')}>
+          <Icon name="ellipsis-h" size={24} color="#000000" />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: 'lightgrey',
+    padding: 15,
+    borderRadius: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: 20,
+    width: '80%',
+    left: 45,
+  },
+  itemLeft: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    flexWrap: 'wrap',
+  },
+  square: {
+    width: 32, // Or whatever size you want
+    height: 32, // Or whatever size you want
+    backgroundColor: '#ADD8E6', // Or whatever color you want
+    justifyContent: 'center', // Centers children vertically
+    alignItems: 'center',
+    borderRadius: 9,
+    paddingRight: 1,
+  },
+  itemText: {
+    maxWidth: '80%',
+    paddingRight: 50,
+  },
+  menu: {
+    paddingRight: 1,
+  },
+});
+
+export default Drink;
